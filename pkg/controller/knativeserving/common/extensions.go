a682f4f3f9f9225ec25469e1a503e368b3eb9d5e
Jim Crossley <jim@crossleys.org>
2019-08-22 17:47:17 -0400

    Minor refactoring

--- pkg/controller/knativeserving/common/extensions.go
+++ pkg/controller/knativeserving/common/extensions.go
@@ -3,7 +3,6 @@ package common
 import (
 	mf "github.com/jcrossley3/manifestival"
 	servingv1alpha1 "github.com/openshift-knative/knative-serving-operator/pkg/apis/serving/v1alpha1"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
@@ -43,14 +42,7 @@ func (exts Extensions) Transform(instance *servingv1alpha1.KnativeServing, schem
 		result = append(result, extension.Transformers...)
 	}
 	// Let any config in instance override everything else
-	return append(result, func(u *unstructured.Unstructured) error {
-		if u.GetKind() == "ConfigMap" {
-			if data, ok := instance.Spec.Config[u.GetName()[len(`config-`):]]; ok {
-				UpdateConfigMap(u, data, log)
-			}
-		}
-		return nil
-	})
+	return append(result, configFromResource(instance))
 }
 
 func (exts Extensions) PreInstall(instance *servingv1alpha1.KnativeServing) error {
--- pkg/controller/knativeserving/common/transform.go
+++ pkg/controller/knativeserving/common/transform.go
@@ -4,6 +4,7 @@ import (
 	"os"
 
 	mf "github.com/jcrossley3/manifestival"
+	servingv1alpha1 "github.com/openshift-knative/knative-serving-operator/pkg/apis/serving/v1alpha1"
 	appsv1 "k8s.io/api/apps/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -21,7 +22,7 @@ func replaceImageFromEnvironment(prefix string, scheme *runtime.Scheme) mf.Trans
 				containers := deploy.Spec.Template.Spec.Containers
 				for i, container := range containers {
 					if container.Name == u.GetName() && container.Image != image {
-						log.Info("Replacing", "deployment", container.Name, "image", image)
+						log.Info("Replacing", "deployment", container.Name, "image", image, "previous", container.Image)
 						containers[i].Image = image
 						break
 					}
@@ -29,6 +30,17 @@ func replaceImageFromEnvironment(prefix string, scheme *runtime.Scheme) mf.Trans
 				if err := scheme.Convert(deploy, u, nil); err != nil {
 					return err
 				}
+			}
+		}
+		return nil
+	}
+}
+
+func configFromResource(instance *servingv1alpha1.KnativeServing) mf.Transformer {
+	return func(u *unstructured.Unstructured) error {
+		if u.GetKind() == "ConfigMap" {
+			if data, ok := instance.Spec.Config[u.GetName()[len(`config-`):]]; ok {
+				UpdateConfigMap(u, data, log)
 			}
 		}
 		return nil
